de1ac44235cdeee4ab9521bb15f7b7940fbcf3ef
joabefilipe <joabefilipe@gmail.com>
2026-01-14 02:13:09 -0300

    tabela populada e login de teste

--- src/main/java/com/example/dasafio_tecnico/service/TokenService.java
+++ src/main/java/com/example/dasafio_tecnico/service/TokenService.java
@@ -15,7 +15,7 @@ import java.time.ZoneOffset;
 @Service
 public class TokenService {
 
-    @Value("${api.security.token.scret}")
+    @Value("${api.security.token.secret}")
     private String secret;
 
     public String genereteToken(Usuarios usuarios){
